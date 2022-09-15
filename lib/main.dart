ae171f2fcbf9763343a539b2e0c9ef01d1eb8c31
BrightDV <92821484+BrightDV@users.noreply.github.com>
2022-09-15 21:05:09 +0200

    remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,6 @@
 //import 'package:flutlab_logcat/flutlab_logcat.dart';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:boxbox/helpers/bottom_navigation_bar.dart';
