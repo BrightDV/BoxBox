85add817a89b92ba6792096c6db7c746f1034c8d
BrightDV <92821484+BrightDV@users.noreply.github.com>
2025-04-15 16:12:32 +0200

    feat(map): use CancellableNetworkTileProvider

--- lib/Screens/circuit_map_screen.dart
+++ lib/Screens/circuit_map_screen.dart
@@ -24,6 +24,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:boxbox/l10n/app_localizations.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:boxbox/helpers/circuit_points.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -150,6 +151,7 @@ class _MarkersPageState extends State<MarkersPage> {
             ),
             TileLayer(
               urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+              tileProvider: CancellableNetworkTileProvider(),
             ),
             PolylineLayer<Object>(
               polylines: snapshot.hasData ? snapshot.data! : [],
