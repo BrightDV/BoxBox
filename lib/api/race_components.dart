50dd9a7237e551c98d4bb1b31df5655ff3be59cb
BrightDV <92821484+BrightDV@users.noreply.github.com>
2025-07-06 11:01:56 +0200

    fix(notification): should fix timezone issue

--- lib/api/race_components.dart
+++ lib/api/race_components.dart
@@ -363,7 +363,7 @@ class RacesList extends StatelessWidget {
             hour: sessionDate.hour,
             day: sessionDate.day,
             month: sessionDate.month,
-            timeZone: 'GMT${session['gmtOffset']}',
+            timeZone: 'GMT+00:00',
           ),
         );
       }
